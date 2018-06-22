04123c271b56f9b11cdf9d75f5de6966cb572db4
wz2cool <wz2cool@live.cn>
2018-06-22 16:58:14 +0800

    add validateEmail

--- src/utils/regex.utils.ts
+++ src/utils/regex.utils.ts
@@ -9,4 +9,28 @@ export class RegexUtils {
   public static escapeRegExp(str: string): string {
     return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
   }
+
+  /**
+   * 3 to 16 letters, numbers, underscores, or hyphens
+   * @param username
+   */
+  public static validateUsername(username: string): boolean {
+    const regex = /^[a-z0-9_-]{3,16}$/;
+    return regex.test(username);
+  }
+
+  /**
+   * Minimum eight characters, at least one letter and one number:
+   * @param password
+   */
+  public static validatePassword(password: string): boolean {
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return regex.test(password);
+  }
+
+  public static validateEmail(email: string): boolean {
+    // from: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return regex.test(email);
+  }
 }
--- test/utils/regex.utils.test.ts
+++ test/utils/regex.utils.test.ts
@@ -14,4 +14,52 @@ describe(".RegexUtils", () => {
       expect(`\\/path\\/to\\/resource\\.html\\?search=query`).to.be.eq(result);
     });
   });
+  describe("#validateUsername", () => {
+    it("should return true if value is wz2cool", () => {
+      const testValue = "wz2cool";
+      const result = RegexUtils.validateUsername(testValue);
+      expect(true).to.be.eq(result);
+    });
+    it("should return false if value is WZ2COOL", () => {
+      const testValue = "WZ2COOL";
+      const result = RegexUtils.validateUsername(testValue);
+      expect(false).to.be.eq(result);
+    });
+    it("should return false if value is wz base less than 3", () => {
+      const testValue = "wz";
+      const result = RegexUtils.validateUsername(testValue);
+      expect(false).to.be.eq(result);
+    });
+  });
+  describe("#validatePassword", () => {
+    it("should return true if value is password123", () => {
+      const testValue = "password123";
+      const result = RegexUtils.validatePassword(testValue);
+      expect(true).to.be.eq(result);
+    });
+    it("should return true if value is passworddddddd", () => {
+      // no number
+      const testValue = "passworddddddd";
+      const result = RegexUtils.validatePassword(testValue);
+      expect(false).to.be.eq(result);
+    });
+    it("should return true if value is 1234567890", () => {
+      // no string
+      const testValue = "1234567890";
+      const result = RegexUtils.validatePassword(testValue);
+      expect(false).to.be.eq(result);
+    });
+  });
+  describe("#validateEmail", () => {
+    it("should return true if value is aaa@live.cn", () => {
+      const testValue = "aaa@live.cn";
+      const result = RegexUtils.validateEmail(testValue);
+      expect(true).to.be.eq(result);
+    });
+    it("should return false if value is aaa", () => {
+      const testValue = "aaa";
+      const result = RegexUtils.validateEmail(testValue);
+      expect(false).to.be.eq(result);
+    });
+  });
 });
