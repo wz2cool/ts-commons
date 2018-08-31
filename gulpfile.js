51321137a21cb858e1e1d5f2ba84fd783311be56
wz2cool <wz2cool@live.cn>
2018-09-01 07:28:09 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
 var gulp = require("gulp");
+var browserify = require("gulp-browserify");
+var clean = require("gulp-clean");
+var concat = require("gulp-concat");
+var sourcemaps = require("gulp-sourcemaps");
 var ts = require("gulp-typescript");
-const sourcemaps = require("gulp-sourcemaps");
-var tsProject = ts.createProject("tsconfig.json");
 var uglify = require("gulp-uglify");
-var merge = require('merge-stream');
 
+gulp.task("default", ["min-js"]);
 
-gulp.task('default', function () {
-    var tsProject = ts.createProject('tsconfig.json');
-    var tsResult = gulp.src(['src/**/*.ts'])
-      .pipe(sourcemaps.init())
-      .pipe(tsProject());
-    return merge(tsResult, tsResult.js)
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./dist/src'));
-  });
+gulp.task("clean", function() {
+  return gulp.src(["./tmp/", "./dist/"], { read: false }).pipe(clean());
+});
+
+gulp.task("transpile-ts", ["clean"], function() {
+  var tsproject = ts.createProject("./tsconfig.json");
+  return tsproject
+    .src()
+    .pipe(sourcemaps.init())
+    .pipe(tsproject())
+    .js.pipe(sourcemaps.write("./sourcemaps"))
+    .pipe(gulp.dest("./tmp"));
+});
+
+gulp.task("min-js", ["transpile-ts"], function() {
+  return gulp
+    .src("./tmp/ts-commons.js")
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(concat("ts-commons.min.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist"));
+});
