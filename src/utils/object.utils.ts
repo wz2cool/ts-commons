f9323c8574d2d246d2fd361322a55e4b704b9426
wz2cool <wz2cool@live.cn>
2022-02-03 15:44:45 +0800

    add

--- src/utils/object.utils.ts
+++ src/utils/object.utils.ts
@@ -230,4 +230,26 @@ export class ObjectUtils {
   public static hasValue<T>(object: T): object is NonNullable<T> {
     return !this.isNullOrUndefined(object);
   }
+
+  /**
+   * if test is true, execute action
+   * @param test whether is true
+   * @param action action
+   */
+  public static ifTrue(test: boolean, action: () => void): void {
+    if (test) {
+      action();
+    }
+  }
+
+  /**
+   * if test is false, execute action
+   * @param test whether is false
+   * @param action action
+   */
+  public static ifFalse(test: boolean, action: () => void): void {
+    if (!test) {
+      action();
+    }
+  }
 }
